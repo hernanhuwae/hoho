7372a98748dac52f234e5155a437dcc487cb373f
hernanhuwae <martinushuwae014@gmail.com>
2024-10-21 15:42:04 +0700

    v4

--- modals/category.modals.ts
+++ modals/category.modals.ts
@@ -1,14 +1,10 @@
 import { Document, model, models, Schema } from "mongoose";
 
-export interface ICategory extends Document{
-    _id:String,
-    name:string
-}
 
 const CategorySchema= new Schema({
     name:{type: String, required:true, unique: true}
 })
 
-const Category= models.Category || model('Category', CategorySchema)
+const Category= models?.Category || model('Category', CategorySchema)
 
 export default Category
